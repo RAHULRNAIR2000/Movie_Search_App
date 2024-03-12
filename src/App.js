0102d3139014f16e3bbb6e07661576ad2d3a4c58
RAHULRNAIR2000 <rahulrnairsreemangalam@gmail.com>
2024-03-12 11:42:08 +0530

    third  commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 const API_URL = "http://www.omdbapi.com?apikey=b391aae7";
 
-const movie1 = {
-  Title: "Italian Spiderman",
-  Year: "2007",
-  imdbID: "tt2705436",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-};
+// const movie1 = {
+//   Title: "Italian Spiderman",
+//   Year: "2007",
+//   imdbID: "tt2705436",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+// };
 
 const App = () => {
   const [movies, setMovies] = useState([]);
